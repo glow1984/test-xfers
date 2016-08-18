e39666f9dfaf67e58bce1dd81d2e60160daa77d1
glow1984 <glow1984@yahoo.com>
2016-08-18 16:13:17 +0800

    Add files via upload

--- xfers_sandbox.js
+++ xfers_sandbox.js
@@ -306,7 +306,7 @@ app.post('/payment_notification', function(requ, resp) {
 	});
 	
 	console.log("request: ",data);
-	//resp.sendStatus(200);
+	resp.sendStatus(200);
 	
 	var options = {
 		  host: 'sandbox.xfers.io',
